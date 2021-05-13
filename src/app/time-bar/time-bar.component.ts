9077befe6d275c7ccd84ec4fca4f19b6639fa047
Manuel Schüpbach <manuel.schuepbach1@students.unibe.ch>
2021-05-13 12:46:23 +0200

    add timeBar that shows the time of shown measurments

--- src/app/time-bar/time-bar.component.ts
+++ src/app/time-bar/time-bar.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Control, DomUtil, Map } from 'leaflet';
+import { AstraCacheService } from '../services/data/astra/astra-cache.service';
 
 @Component({
   selector: 'app-time-bar',
@@ -11,7 +12,7 @@ export class TimeBarComponent implements OnInit {
   @Input() map: Map;
   timebar: Control;
 
-  constructor() { }
+  constructor(private _astraCache: AstraCacheService) { }
 
   ngOnInit(): void {
     let TimeBar = Control.extend({
@@ -22,8 +23,11 @@ export class TimeBarComponent implements OnInit {
     })
     this.timebar = new TimeBar({
       position: 'bottomright'
-    }).addTo(this.map)
-    
+    }).addTo(this.map) 
+  }
+
+  date(): Date{
+    return this._astraCache.getMeasurementDate();
   }
 
 }
